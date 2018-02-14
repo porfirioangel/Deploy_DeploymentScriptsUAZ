c39c4d8a41b039dd15a08d5b11d2c3dd8a98b76a
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2018-02-14 14:55:28 +0000

    Crea usuarios sin interacción

--- crear_usuario_hook.sh
+++ crear_usuario_hook.sh
@@ -17,21 +17,19 @@ if [ $UID -ne 0 ]; then
 fi
 
 read -p "Ingresa el nombre de usuario: " USUARIO
+read -p "Ingresa la contraseña: " PASSWORD
 
-adduser $USUARIO
+echo "Creando el usuario $USUARIO..."
+useradd -m -s /bin/bash $USUARIO
+echo $USUARIO:$PASSWORD | chpasswd
 
 USER_HTML=/var/www/html/$USUARIO
-
 echo "Creando directorio $USER_HTML"
-
 mkdir $USER_HTML
-
 chown $USUARIO:$USUARIO $USER_HTML
 
 USER_PUBLIC_HTML=/home/$USUARIO/public_html
-
 echo "Creando enlace simbólico de $USER_PUBLIC_HTML en $USER_HTML"
-
 ln -s $USER_HTML $USER_PUBLIC_HTML
 
 echo "Proceso terminado"
