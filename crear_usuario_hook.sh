cb425fd082dcfba4df8c0e0933800a04c597bac4
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2018-02-14 14:27:09 +0000

    Implementa script para crear usuarios

---
+++ crear_usuario_hook.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Ver información de los usuarios
+# cat /etc/passwd
+# Eliminar usuario
+# sudo userdel -r porfirioads
+# sudo rm -r /var/mail/porfirioads
+
+echo "+-------------------------------------+"
+echo "| Crear usuario para establecer hooks |"
+echo "+-------------------------------------+"
+echo ""
+
+if [ $UID -ne 0 ]; then
+    echo "Se requieren permisos de administrador"
+    exit
+fi
+
+read -p "Ingresa el nombre de usuario: " USUARIO
+
+adduser $USUARIO
+
+USER_HTML=/var/www/html/$USUARIO
+
+echo "Creando directorio $USER_HTML"
+
+mkdir $USER_HTML
+
+chown $USUARIO:$USUARIO $USER_HTML
+
+USER_PUBLIC_HTML=/home/$USUARIO/public_html
+
+echo "Creando enlace simbólico de $USER_PUBLIC_HTML en $USER_HTML"
+
+ln -s $USER_HTML $USER_PUBLIC_HTML
+
+echo "Proceso terminado"
